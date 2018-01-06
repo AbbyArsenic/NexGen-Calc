9da92dde8726009e0756eb98acdb9f900493059c
Abby Johnson <brit13@gmail.com>
2018-01-06 03:18:16 -0700

    onchange added for airport data

---
+++ ngc/src/pages/6FlightPlan/Arrive.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export const Arrive = () => (
+  <div>
+    Departing Airport:<select name="arrive">
+      <option value="">---</option>
+      <option value="KIWA">KIWA (AZ-Gateway)</option>
+    </select>
+    <h2>Airport of Arrival data</h2>
+    <table>
+      <tbody>
+        <tr>
+          <th>Elevation: </th>
+          <td>
+            <input name="elevationArrive" />
+          </td>
+        </tr>
+        <tr>
+          <th>Headwind: </th>
+          <td>
+            <input name="headwindArrive" />
+          </td>
+        </tr>
+        <tr>
+          <th>Takeoff: </th>
+          <td>
+            <input name="TakeoffArrive" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
+
+export default Arrive;
--- ngc/src/pages/6FlightPlan/FlightPlan.js
+++ ngc/src/pages/6FlightPlan/FlightPlan.js
@@ -3,67 +3,107 @@ import { Link } from 'react-router-dom';
 
 import { NextButton } from '../../components/Buttons';
 
-const FlightPlan = () => (
-	<div>
-		<div>
-			<h2>Airport of Departure data</h2>
-			<table>
-				<tbody>
-					<tr>
-						<th>Elevation: </th>
-						<td>
-							<input name="elevationDepart" />
-						</td>
-					</tr>
-					<tr>
-						<th>Headwind: </th>
-						<td>
-							<input name="headwindDepart" />
-						</td>
-					</tr>
-					<tr>
-						<th>Takeoff: </th>
-						<td>
-							<input name="elevationDepart" />
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-		<hr />
-		<div>
-			<h2>Airport of Arrival data</h2>
-			<table>
-				<tbody>
-					<tr>
-						<th>Elevation: </th>
-						<td>
-							<input name="elevationArive" />
-						</td>
-					</tr>
-					<tr>
-						<th>Headwind: </th>
-						<td>
-							<input name="headwindArive" />
-						</td>
-					</tr>
-					<tr>
-						<th>Takeoff: </th>
-						<td>
-							<input name="elevationArive" />
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-		<hr />
-		<div>
-			<button>Clear</button>
-			<Link to={"/TakeOffData"}>
-				<NextButton />
-			</Link>
-		</div>
-	</div>
-);
+class FlightPlan extends React.Component {
+	state = {
+		departSearch: '',
+		departData: [],
+		arriveSearch: '', 
+		arriveData: []
+	}; 
+
+	//function to handle onChange for dropdown
+	change = e => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+
+		/*const airportData = e.target.name + "Data";
+		axios.get(`http://avwx.rest/api/metar/${value}?options=info`)
+      .then(res => this.setState({ airportData: res.data }))
+      .catch(err => console.log(err));*/
+	}
+
+	render() {
+		return (
+			<div>
+				<div>
+					<h2>Airport of Departure: </h2>
+					<select 
+						name="departSearch"
+						value={this.state.departSearch}
+						onChange={this.change}>
+						<option value="">---</option>
+						<option value="KIWA">KIWA (AZ-Gateway)</option>
+					</select>
+					<h2>Airport data</h2>
+					<table>
+						<tbody>
+							<tr>
+								<th>Elevation: </th>
+								<td>
+									<input name="elevationDepart" value={this.state.departSearch} />
+								</td>
+							</tr>
+							<tr>
+								<th>Headwind: </th>
+								<td>
+									<input name="headwindDepart" />
+								</td>
+							</tr>
+							<tr>
+								<th>Takeoff: </th>
+								<td>
+									<input name="elevationDepart" />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<hr />
+				<div>
+					<h2>Airport of Arrival: </h2>
+					<select 
+						name="arriveSearch"
+						value={this.state.arriveSearch}
+						onChange={this.change}>
+						<option value="">---</option>
+						<option value="KIWA">KIWA (AZ-Gateway)</option>
+					</select>
+					<h2>Airport data</h2>
+					<table>
+						<tbody>
+							<tr>
+								<th>Elevation: </th>
+								<td>
+									<input name="elevationArive" />
+								</td>
+							</tr>
+							<tr>
+								<th>Headwind: </th>
+								<td>
+									<input name="headwindArive" value={this.state.arriveSearch} />
+								</td>
+							</tr>
+							<tr>
+								<th>Takeoff: </th>
+								<td>
+									<input name="elevationArive" />
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<hr />
+				<div>
+					<button>Clear</button>
+					<Link to={"/TakeOffData"}>
+						<NextButton />
+					</Link>
+				</div>
+			</div>
+		)
+	}
+}
 
 export default FlightPlan;
