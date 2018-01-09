f212f8e7bfbfd02a0cbbd8b6d856711e18e593cc
Abby Johnson <brit13@gmail.com>
2018-01-08 22:32:08 -0700

    Bootstrap CDN, header/nav cleaned up

--- ngc/src/components/Header.js
+++ ngc/src/components/Header.js
@@ -1,25 +1,58 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 
 const Header = () => {
-    return (
-        <header>
-            <h1>NexGen Calc</h1>
-            <NavLink to="/" activeClassName="is-active" exact={true}>| Home Page |</NavLink>
-            <NavLink to="/SignUp" activeClassName="is-active">| Sign Up |</NavLink>
-            <NavLink to="/TermsOfService" activeClassName="is-active">| ToS |</NavLink>
-            <NavLink to="/Login" activeClassName="is-active">| Login |</NavLink>
-            <NavLink to="/ForgotPassword" activeClassName="is-active">| Password Recovery |</NavLink>
-        	<NavLink to="/FlightPlan" activeClassName="is-active">| Flight Plan |</NavLink>
-        	<NavLink to="/TakeOffData" activeClassName="is-active">| Take Off Data |</NavLink>
-        	<NavLink to="/LandingData" activeClassName="is-active">| Landing Data |</NavLink>
-        	<NavLink to="/TOLD-Card" activeClassName="is-active">| TOLD Card |</NavLink>
-        	
-        	<br />
-        	<NavLink to="/asdf" activeClassName="is-active">| 404 Page |</NavLink>
-        </header>
-    );
+  return(
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">NexGen Calc</a>
+        </div>
+        <ul class="nav navbar-nav">
+          
+        <li>
+          <NavLink to="/TermsOfService" 
+            activeClassName="is-active">ToS
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/FlightPlan" 
+            activeClassName="is-active">Flight Plan
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/TakeOffData" 
+            activeClassName="is-active">Take Off Data
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/LandingData" 
+            activeClassName="is-active">Landing Data
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/TOLD-Card" 
+            activeClassName="is-active">TOLD Card
+          </NavLink>
+        </li>            
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <NavLink to="/SignUp" activeClassName="is-active">
+              <span class="glyphicon glyphicon-user"></span> Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Login" activeClassName="is-active">
+              <span class="glyphicon glyphicon-log-in"></span> Login
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- ngc/src/components/utils/API.js
+++ ngc/src/components/utils/API.js
@@ -1,8 +1,45 @@
-import axios from "axios";
+import request from 'superagent'
 
-// It accepts a "query" or term to search the airport data api for
-export default {
-  getAirportData: function(query) {
-    return axios.get("/api/airport", { params: { q: query}});
+const airportData = store => next => action => {
+  /*
+  Pass all actions through by default
+  */
+  next(action)
+  switch (action.type) {
+  case 'GET_AIRPORT_DATA':
+    /*
+    In case we receive an action to send an API request, send the appropriate request
+    */
+    request
+      .get('http://avwx.rest/api/metar/KIWA?options=info')
+      .end((err, res) => {
+        if (err) {
+          /*
+          in case there is any error, dispatch an action containing the error
+          */
+          return next({
+            type: 'GET_AIRPORT_DATA_ERROR',
+            err
+          })
+        }
+        const data = JSON.parse(res.text)
+        /*
+        Once data is received, dispatch an action telling the application
+        that data was received successfully, along with the parsed data
+        */
+        next({
+          type: 'GET_AIRPORT_DATA_RECEIVED',
+          data
+        })
+      })
+    break
+  /*
+  Do nothing if the action does not interest us
+  */
+  default:
+    break
   }
+
 };
+
+export default airportData
