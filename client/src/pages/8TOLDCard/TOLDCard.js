98bbb39b263f06299b9b6d4adec84bf329ab2468
jtmPC <jon87tyler@gmail.com>
2018-01-13 00:39:06 -0700

    added units of measurement for TOLD card

--- client/src/pages/8TOLDCard/TOLDCard.js
+++ client/src/pages/8TOLDCard/TOLDCard.js
@@ -32,20 +32,20 @@ const TOLDCard = ({ calculatedValues: val }) => (
             </th>
           </tr>
           <tr>
-            <td>P-ALT: <span className='TOLDvalue'>{val.pressureAltitudeTakeoff}</span></td>
-            <td>D-ALT: <span className='TOLDvalue'>{val.densityAltitudeTakeoff}</span></td>
+            <td>P-ALT: <span className='TOLDvalue'>{val.pressureAltitudeTakeoff} FT</span></td>
+            <td>D-ALT: <span className='TOLDvalue'>{val.densityAltitudeTakeoff} FT</span></td>
           </tr>
           <tr>
-            <td>Liftoff Speed: <span className='TOLDvalue'>{val.liftoffSpeed}</span></td>
-            <td>Liftoff Speed (50'): <span className='TOLDvalue'>{val.liftoffSpeed50Barrier}</span></td>
+            <td>Liftoff Speed: <span className='TOLDvalue'>{val.liftoffSpeed} KTS</span></td>
+            <td>Liftoff Speed (50'): <span className='TOLDvalue'>{val.liftoffSpeed50Barrier} KTS</span></td>
           </tr>
           <tr>
-            <td>T.O. Roll: <span className='TOLDvalue'>{val.takeoffRoll}</span></td>
-            <td>T.O. Roll (50'): <span className='TOLDvalue'>{val.takeoffDistance50Barrier}</span></td>
+            <td>T.O. Roll: <span className='TOLDvalue'>{val.takeoffRoll} FT</span></td>
+            <td>T.O. Roll (50'): <span className='TOLDvalue'>{val.takeoffDistance50Barrier} FT</span></td>
           </tr>
           <tr>
-            <td>ACC/Stop: <span className='TOLDvalue'>{val.accelerateStop}</span></td>
-            <td>Climb (FPM): <span className='TOLDvalue'>{val.climbRate}</span></td>
+            <td>ACC/Stop: <span className='TOLDvalue'>{val.accelerateStop} FT</span></td>
+            <td>Climb (FPM): <span className='TOLDvalue'>{val.climbRate} FPM</span></td>
           </tr>
           <tr>
             <th colSpan="4" className="TOLDheader">
@@ -53,15 +53,15 @@ const TOLDCard = ({ calculatedValues: val }) => (
             </th>
           </tr>
           <tr>
-            <td>P-ALT: <span className='TOLDvalue'>{val.pressureAltitudeLanding}</span></td>
-            <td>D-ALT: <span className='TOLDvalue'>{val.densityAltitudeLanding}</span></td>
+            <td>P-ALT: <span className='TOLDvalue'>{val.pressureAltitudeLanding}  FT</span></td>
+            <td>D-ALT: <span className='TOLDvalue'>{val.densityAltitudeLanding}  FT</span></td>
           </tr>
           <tr>
-            <td>LND Dist: <span className='TOLDvalue'>{val.landingRoll}</span></td>
-            <td>LND Dist. (50'): <span className='TOLDvalue'>{val.landingDistance50Barrier}</span></td>
+            <td>LND Dist: <span className='TOLDvalue'>{val.landingRoll}  FT</span></td>
+            <td>LND Dist. (50'): <span className='TOLDvalue'>{val.landingDistance50Barrier}  FT</span></td>
           </tr>
           <tr>
-            <td>APP Speed: <span className='TOLDvalue'>{val.approachSpeed}</span></td>
+            <td>APP Speed: <span className='TOLDvalue'>{val.approachSpeed} KTS</span></td>
           </tr>
           <tr>
             <th colSpan="4" className="TOLDheader">
@@ -83,7 +83,7 @@ const TOLDCard = ({ calculatedValues: val }) => (
     <div className="hidePrint">
       <Link to="./TOLDreview"><Button text="Back" /></Link>{" "}
       <button className="btn" onClick={printTOLD}>
-        <span class="glyphicon glyphicon-print"></span> Print
+        <span className="glyphicon glyphicon-print"></span> Print
       </button>
   	</div>
   </div>
