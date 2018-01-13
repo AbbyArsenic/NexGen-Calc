67993540a8de3d52d8e95209d776712e8c39a7bc
jtmPC <jon87tyler@gmail.com>
2018-01-13 01:31:09 -0700

    fixed api url to be https

--- client/src/utils/APIs/weatherAPI.js
+++ client/src/utils/APIs/weatherAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
 
     getWx: (airport) => {
-        const url = `http://avwx.rest/api/metar/${airport}?options=info`;
+        const url = `https://avwx.rest/api/metar/${airport}?options=info`;
         return axios.get(url);
     }
     
